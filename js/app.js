685402727d01b63dfcc36cb0580176783fcc350d
Rajat <razat249@gmail.com>
2016-09-18 00:03:25 +0530

    remove navbar.html

--- js/app.js
+++ js/app.js
@@ -1,38 +1,41 @@
-'use strict';
+(function(){
+	'use strict';
 
-var app = angular.module('onePush', []);
+	var app = angular.module('onePush', []);
 
-app.controller('ListCtrl', ['$http', '$scope', function($http, $scope) {
-	$scope.loading = true;
-	$scope.serverError = false;
-	
-	$http.get('https://hackerearth.0x10.info/api/one-push?type=json&query=list_websites').then(function(data) {
-		$scope.peopleList = data.data.websites;
-		$scope.loading = false;
-		$scope.noOfSites = data.data.websites.length;
-	}, function() {
-		$scope.loading = false;
-		$scope.serverError = true;
-		$scope.errorMessage = "Something went wrong. Try reloading :)";
-	});
-}]);
-
-app.controller('FormCtrl', ['$http', '$scope', function($http, $scope) {
-	$scope.submit = function() {
-		var person = $scope.person;
-		$scope.success = false;
-		$http.get('https://hackerearth.0x10.info/api/one-push?type=json&query=push&title='+ person.title +'&url='+person.url+'&tag='+person.tag).then(
-		function(data) {
-			if (data.data.status == 200) {
-				$scope.success = true
-				$scope.person = {};
-				$scope.postStatus = "Post Successful! To post another website, come back after one hour :)"			
-			} else {
-				$scope.postStatus = "Can not post, try again later!";
-				$scope.person = {};
-			}
-		}, function(data) {
-			console.log("Server Error");
+	//controller for main section listing.
+	app.controller('ListCtrl', ['$http', '$scope', function($http, $scope) {
+		$scope.loading = true;
+		$scope.serverError = false;
+		$http.get('https://hackerearth.0x10.info/api/one-push?type=json&query=list_websites').then(function(data) {
+			$scope.peopleList = data.data.websites;
+			$scope.loading = false;
+			$scope.noOfSites = data.data.websites.length;
+		}, function() {
+			$scope.loading = false;
+			$scope.serverError = true;
+			$scope.errorMessage = "Something went wrong. Try reloading :)";
 		});
-	}
-}]);
+	}]);
+
+	//controller for form.
+	app.controller('FormCtrl', ['$http', '$scope', function($http, $scope) {
+		$scope.submit = function() {
+			var person = $scope.person;
+			$scope.success = false;
+			$http.get('https://hackerearth.0x10.info/api/one-push?type=json&query=push&title='+ person.title +'&url='+person.url+'&tag='+person.tag).then(
+			function(data) {
+				if (data.data.status == 200) {
+					$scope.success = true
+					$scope.person = {};
+					$scope.postStatus = "Post Successful! To post another website, come back after one hour :)";
+				} else {
+					$scope.postStatus = "Can not post, try again later!";
+					$scope.person = {};
+				}
+			}, function(data) {
+				console.log("Server Error");
+			});
+		}
+	}]);
+})();
